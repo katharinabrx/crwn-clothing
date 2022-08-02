4da8f766338add1404d910eacaf64c98574e21fd
Katharina Brüggemann <mail@katharina.dev>
2022-08-02 16:04:48 +0200

    switch to redux logger in development only

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,9 @@
-import { compose, createStore, applyMiddleware } from "redux";
-import { persistStore, persistReducer } from "redux-persist";
+import { applyMiddleware, compose, createStore } from "redux";
+import logger from "redux-logger";
+import { persistReducer, persistStore } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+
 import { rootReducer } from "./root-reducer";
-
-const loggerMiddleware = (store) => (next) => (action) => {
-  if (!action.type) {
-    return next(action);
-  }
-
-  console.log("type: ", action.type);
-  console.log("payload: ", action.payload);
-  console.log("currentState: ", store.getState());
-
-  next(action);
-
-  console.log("next state: ", store.getState());
-};
 
 const persistConfig = {
   key: "root",
@@ -25,7 +13,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [loggerMiddleware];
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(Boolean);
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
