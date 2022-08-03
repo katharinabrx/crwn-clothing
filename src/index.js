16882661e214743a62de02a4bdfd35e6418f0988
Katharina Brüggemann <mail@katharina.dev>
2022-08-03 17:29:43 +0200

    added stripe payment to website

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import "./index.scss";
 
+import { Elements } from "@stripe/react-stripe-js";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { store, persistor } from "./store/store";
-import { PersistGate } from "redux-persist/integration/react";
+import { persistor, store } from "./store/store";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,7 +18,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -4,6 +4,8 @@ import CheckoutItem from "../../components/checkout-item/checkout-item.component
 import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
 import { CheckoutContainer, CheckoutHeader, HeaderBlock, Total } from "./checkout.styles";
 
+import PaymentForm from "../../components/payment-form/payment-form.component";
+
 const Checkout = () => {
   const cartItems = useSelector(selectCartItems);
   const cartTotal = useSelector(selectCartTotal);
@@ -31,6 +33,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <Total>Total: ${cartTotal}</Total>
+      <PaymentForm />
     </CheckoutContainer>
   );
 };
