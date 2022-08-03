1bd7cee998e980d8e434f3e2f59a1f62cba80a23
Katharina Brüggemann <mail@katharina.dev>
2022-08-03 18:34:33 +0200

    fix: global style

--- src/global.styles.js
+++ src/global.styles.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle()`
+export const GlobalStyle = createGlobalStyle`
 * {
   box-sizing: border-box;
 }
@@ -20,4 +20,5 @@ body {
 a {
   text-decoration: none;
   color: black;
+  }
 `;
